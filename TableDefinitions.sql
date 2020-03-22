2ec441526c9be8b5ceed6ac0d6136fc04aa9775b
Gafurul Islam Kazi <32606756+gafurulkazi98@users.noreply.github.com>
2020-03-21 20:12:09 -0400

    Add files via upload

---
+++ TableDefinitions.sql
@@ -0,0 +1,78 @@
+﻿
+CREATE TABLE Person (
+        username VARCHAR(32),
+        password VARCHAR(64),
+        firstName VARCHAR(32),
+        lastName VARCHAR(32),
+        email VARCHAR(32),
+        PRIMARY KEY (username)
+);
+
+CREATE TABLE Photo (
+        pID INT AUTO_INCREMENT,
+        postingDate DATETIME,
+        filepath BLOB(255),
+        allFollowers BOOLEAN,
+        caption VARCHAR(1000),
+        posterUsername VARCHAR(32),
+        PRIMARY KEY (pID),
+        FOREIGN KEY (posterUsername) REFERENCES Person (username)
+);
+
+CREATE TABLE FriendGroup (
+        groupName VARCHAR(32),
+        creatorUsername VARCHAR(32),
+        description VARCHAR(1000),
+        PRIMARY KEY (groupName, creatorUsername),
+        FOREIGN KEY (creatorUsername) REFERENCES Person (username)
+);
+
+CREATE TABLE React (
+        reactorUsername VARCHAR(32),
+        pID INT,
+        reactionTime DATETIME,
+        comment VARCHAR(1000),
+        emoji BLOB(32),
+	PRIMARY KEY (username, pID),
+        FOREIGN KEY (pID) REFERENCES Photo (pID),
+        FOREIGN KEY (reactorUsername) REFERENCES Person (username)
+);
+
+CREATE TABLE Tag (
+        pID INT,
+        taggedUsername VARCHAR(32),
+        tagStatus BOOLEAN,
+	PRIMARY KEY (pID, username),
+        FOREIGN KEY (pID) REFERENCES Photo (pID),
+        FOREIGN KEY (taggedUsername) REFERENCES Person (username)
+);
+
+
+CREATE TABLE Share (
+        pID INT,
+        groupName VARCHAR(32),
+        creatorUsername VARCHAR(32),
+	PRIMARY KEY (pID, groupName, creatorUsername),
+	FOREIGN KEY (groupName, creatorUsername) REFERENCES FriendGroup(groupName, creatorUsername),
+        FOREIGN KEY (pID) REFERENCES Photo (pID)
+);
+
+
+
+CREATE TABLE GroupMember (
+        memberUsername VARCHAR(32),
+        groupName VARCHAR(32),
+	creatorUsername VARCHAR(32),
+        PRIMARY KEY (memberUsername, groupName, creatorUsername),
+        FOREIGN KEY (memberUsername) REFERENCES Person (username),
+        FOREIGN KEY (groupName, creatorUsername) REFERENCES FriendGroup (groupName, creatorUsername)
+);
+
+CREATE TABLE Follow (
+        followerUsername VARCHAR(32),
+        followeeUsername VARCHAR(32),
+        followStatus BOOLEAN,
+        PRIMARY KEY (followerUsername, followeeUsername),
+        FOREIGN KEY (followerUsername) REFERENCES Person (username),
+        FOREIGN KEY (followeeUsername) REFERENCES Person (username)
+);

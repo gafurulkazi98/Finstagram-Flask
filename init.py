2687fdf7e2e56d369ffec3d43ce64f3ab6c46c85
gafurulkazi98 <gafurulkazi98@yahoo.com>
2020-03-24 14:28:32 -0400

    Update init.py

--- init.py
+++ init.py
@@ -5,14 +5,11 @@ Created on Thu Mar 19 16:33:11 2020
 @author: Rafi
 """
 
-#Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 
-#Initialize the app from Flask
 app = Flask(__name__)
 
-#Configure MySQL
 conn = pymysql.connect(host='localhost',
                        port = 3308,
                        user='root',
@@ -21,60 +18,44 @@ conn = pymysql.connect(host='localhost',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
-#Define route for login
 @app.route('/')
 def login():
     return render_template('login.html')
 
-#Define route for register
 @app.route('/register')
 def register():
     return render_template('register.html')
 
-#Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
-    #grabs information from the forms
     username = request.form['username']
     password = request.form['password']
-
-    #cursor used to send queries
+    
     cursor = conn.cursor()
-    #executes query
     query = 'SELECT * FROM person WHERE username = %s and password = %s'
     cursor.execute(query, (username, password))
-    #stores the results in a variable
     data = cursor.fetchone()
     cursor.close()
     if(data):
-        #creates a session for the the user
-        #session is a built in
         session['username'] = username
         return redirect(url_for('home'))
     else:
-        #returns an error message to the html page
         return render_template('login.html', error='Invalid login or username')
 
-#Authenticates the registration
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
-    #grabs information from the forms
     username = request.form['username']
     password = request.form['password']
     vpassword = request.form['vpassword']
     firstName = request.form['firstName']
     lastName = request.form['lastName']
     email = request.form['email']
-
-    #cursor used to send queries
+    
     cursor = conn.cursor()
-    #executes query
     query = 'SELECT * FROM person WHERE username = %s'
     cursor.execute(query, (username))
-    #stores the results in a variable
     data = cursor.fetchone()
     if(data):
-        #If the previous query returns data, then username already exists
         return render_template('register.html', error = "This username is taken")
     else:
         if password!=vpassword:
@@ -83,6 +64,7 @@ def registerAuth():
         cursor.execute(ins, (username, password, firstName, lastName, email))
         conn.commit()
         cursor.close()
+        session['username']=username
         return render_template('home.html',user=username)
     
 @app.route('/home')
@@ -92,11 +74,54 @@ def home():
     except:
         return redirect('/')
     #cursor = conn.cursor();
-    #query = 'SELECT filename FROM person NATURAL JOIN  WHERE username = %s ORDER BY ts DESC'
+    #query = 'SELECT filename FROM follow NATURAL JOIN photo WHERE followerUsername = %s ORDER BY postingDate DESC'
     #cursor.execute(query, (username))
     #data = cursor.fetchall()
     #cursor.close()
     return render_template('home.html',user=username)#,feed=data)
+
+@app.route('/newPost')
+def newPost():
+    try:
+        username=session['username']
+    except:
+        return redirect('/')
+    cursor=conn.cursor()
+    query = 'SELECT groupName,creatorUsername FROM groupMember WHERE memberUsername = %s'
+    cursor.execute(query,(username))
+    data=cursor.fetchall()
+    cursor.close()
+    return render_template('newPost.html',user=username,friendGroups=data)
+
+@app.route('/submitPost', methods=['GET','POST'])
+def submitPost():
+    try:
+        username=session['username']
+    except:
+        return redirect('/')
+    filepath = request.form['filepath']
+    #How to use BLOB datatype
+    caption = request.form['caption']
+    shareWith = request.form['shareWith']
+    cursor=conn.cursor()
+    if shareWith == "allFollowers":
+        ins = 'INSERT INTO photo VALUES(NULL,%s, CURRENT_TIMESTAMP, %s, 1, %s)'
+        cursor.execute(ins,(username,filepath,caption))
+        conn.commit()
+    else:
+        ins = 'INSERT INTO photo VALUES(NULL, %s, CURRENT_TIMESTAMP, %s, 0, %s)'
+        cursor.execute(ins,(username,filepath,caption))
+        conn.commit()
+        query = 'SELECT LAST_INSERT_ID()'
+        pID = cursor.execute(query)
+        #return render_template('debug.html',pID=pID,shareWith=shareWith)
+        groupName = cursor.execute('SELECT %s.groupName',(shareWith))
+        creatorUsername = cursor.execute('SELECT %s.creatorUsername',(shareWith))
+        return render_template('debug.html',gn=groupName,cu=creatorUsername)
+        ins = 'INSERT INTO share VALUE(%s,%s,%s)'
+        cursor.execute(ins,(pID,groupName,creatorUsername))
+    cursor.close()
+    return render_template('home.html',user=username)
 
 @app.route('/friendGroups')
 def friendGroup():
@@ -105,7 +130,7 @@ def friendGroup():
     except:
         return redirect('/')
     cursor = conn.cursor()
-    query = 'SELECT * FROM groupMember WHERE memberUsername = %s'
+    query = 'SELECT * FROM groupMember NATURAL JOIN friendGroup WHERE memberUsername = %s'
     cursor.execute(query,(username))
     data = cursor.fetchall()
     cursor.close()
@@ -127,33 +152,37 @@ def authFriendGroup():
         return redirect('/')
     newGroupName = request.form['newGroupName']
     description = request.form['description']
-    print("ding1")
     cursor = conn.cursor()
-    print("ding2")
     query = 'SELECT groupName,creatorUsername FROM friendGroup WHERE groupName = %s AND creatorUsername = %s'
     cursor.execute(query,(newGroupName,username))
-    print("ding3")
     data = cursor.fetchone()
-    print("ding4")
     error = None
     if(data):
         cursor.close()
         error = 'You already own a friend group with this name'
-        return render_template("newFriendGroup", error=error)
+        return render_template("newFriendGroup.html",user=username, error=error)
     else:
         ins = 'INSERT INTO friendGroup VALUES(%s, %s, %s)'
         cursor.execute(ins, (newGroupName,username,description))
-        print("ding5")
         conn.commit()
-        print("ding6")
         ins = 'INSERT INTO groupMember VALUES(%s, %s, %s)'
         cursor.execute(ins, (newGroupName,username,username))
-        print("ding7")
         conn.commit()
-        print("ding8")
         cursor.close()
-        print("ding9")
         return render_template("friendGroups.html",user=username)
+    
+@app.route('/follows')
+def follows():
+    try:
+        username=session['username']
+    except:
+        return redirect('/')
+    cursor = conn.cursor()
+    query = 'SELECT followeeUsername FROM follow WHERE followeeUsername = %s AND followStatus = 0'
+    cursor.execute(query,(username))
+    newFollowers = cursor.fetchall()
+    cursor.close()
+    return render_template("follows.html",newFollowers)
 
 @app.route('/logout')
 def logout():
@@ -161,8 +190,5 @@ def logout():
     return redirect('/')
 
 app.secret_key = '4pp 53cr37 k3y'
-#Run the app on localhost port 5000
-#debug = True -> you don't have to restart flask
-#for changes to go through, TURN OFF FOR PRODUCTION
 if __name__ == "__main__":
     app.run('127.0.0.1', 5000, debug = True)
